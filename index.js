2afcfb9fcb60ee8fc82a73225140aee5c60e914f
David Beneš <dbenes@gmail.com>
2021-04-21 12:50:29 +0200

    upgraded to tdlib 1.7.3, added alpine linux

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const fs = require('fs')
 const path = require('path')
 const os = require('os')
 
+const VERSION = '1.7.3'
+
+function getDistro() {
+  const descr = fs.readFileSync('/etc/os-release', {encoding:'utf8', flag:'r'})
+  const distro = descr.match(/[^A-Z_]+ID=(.*)/)
+  return distro ? distro[1] : null
+}
+
 module.exports = () => {
   const cwd = path.resolve(__dirname)
-  const lib_path = `${cwd}/bin/${os.platform()}-${os.arch()}/libtdjson.so.1.7.1`
-
+  const lib_path = `${cwd}/bin/${getDistro()}-${os.platform()}-${os.arch()}/libtdjson.so.${VERSION}`
   if (fs.existsSync(lib_path)) {
     return lib_path
   }
